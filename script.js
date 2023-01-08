71e26edc4ff85feb730ebc1ba9724f546b51413b
yuicookie <104888283+yuicookie@users.noreply.github.com>
2023-01-08 19:08:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $("body").mousedown
 // Audioオブジェクトを作成
 let ad = new Audio("audio/xmas.m4a");
 
-// 同意ボタンクリック
+// 同意ボタン押す
 const agree_btn = document.getElementById('agree');
 agree_btn.addEventListener('mousedown', e => {
   document.getElementById("agree").style.display ="none";
@@ -35,7 +35,7 @@ agree_check.addEventListener('mouseup', e => {
   document.getElementById("ok_btn_black").style.display ="block";
 });
 
-// OKボタンクリック
+// OKボタン押す
 const ok_btn = document.getElementById('ok_btn');
 ok_btn.addEventListener('mousedown', e => {
   document.getElementById("ok_btn").style.display ="none";
@@ -53,3 +53,39 @@ let elem_loop = document.getElementById("bgm");
 	ad.loop = true;  // ループ再生
  false;
 }});
+
+
+// リーグランキングボタン押す
+const friend_rank_btn2 = document.getElementById('friend_rank_btn2');
+friend_rank_btn2.addEventListener('mousedown', e => {
+  document.getElementById("friend_rank_btn2").style.display ="none";
+  document.getElementById("league_rank_btn2").style.display ="block";
+})
+// リーグランキングボタン離す
+const league_rank_btn2 = document.getElementById('league_rank_btn2');
+league_rank_btn2.addEventListener('mouseup', e => {
+  document.getElementById("combi_bonus").style.display ="none";
+  document.getElementById("friend_rank_btn1").style.display ="none";
+  document.getElementById("friend_rank").style.display ="none";
+  document.getElementById("league_rank_btn1").style.display ="block";
+  document.getElementById("league_rank").style.display ="block";
+  document.getElementById("medal").style.display ="block";
+});
+
+// 友達ランキングボタン押す
+const league_rank_btn1 = document.getElementById('league_rank_btn1');
+league_rank_btn1.addEventListener('mousedown', e => {
+  document.getElementById("league_rank_btn1").style.display ="none";
+  document.getElementById("friend_rank_btn1").style.display ="block";
+});
+
+// 友達ランキングボタン離す
+const friend_rank_btn1 = document.getElementById('friend_rank_btn1');
+friend_rank_btn1.addEventListener('mouseup', e => {
+  document.getElementById("league_rank_btn2").style.display ="none";
+  document.getElementById("medal").style.display ="none";
+  document.getElementById("league_rank").style.display ="none";
+  document.getElementById("friend_rank").style.display ="block";
+  document.getElementById("friend_rank_btn2").style.display ="block";
+  document.getElementById("combi_bonus").style.display ="block";
+});
